6b75f4b48c3f7ca18da66ccb133ebee95a3e1d0e
WillMelton21 <meltonjw3@gmail.com>
2025-11-04 19:59:45 -0500

    commented out unsued functinos

--- src/parse.c
+++ src/parse.c
@@ -11,18 +11,16 @@
 #include "common.h"
 #include "parse.h"
 
-void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
+//void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
 
-}
+//}
 
-int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
-   return -1;
-}
+//int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
+//}
 
-int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employeesOut) {
+//int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employeesOut) {
 
-   return -1;
-}
+//}
 
 int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees) {
    if (fd < 0) {
